9c03fe29ed45673c7d791330942b2056e2c4bc5a
willi <willi@local>
2026-04-23 09:37:08 -0500

    feat: add config server

---
+++ infra/config-server/src/test/java/com/libwill/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libwill.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
